8b34cde98616247babc31f8e9a9b3458fb701507
bigenergy <andre90094@gmail.com>
2018-10-29 12:23:38 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('#fullpage').fullpage({
+    verticalCentered: false,
+    navigation:false,
+      anchors:['main', 'about', 'portfolio', 'crafting', 'contact', 'promo', 'comments'],
+      menu: false,
+
+  });
+});
